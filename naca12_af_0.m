64eb77a0ecb0ab1f21174d4696138da6c167aa2e
Andrea Gallegati <37024974+andreagalle@users.noreply.github.com>
2019-03-13 11:00:11 +0100

    Add files via upload

---
+++ naca12_af_0.m
@@ -0,0 +1,12 @@
+
+filename       = 'NACA 0012.dat';
+delimiterIn    = ' ';
+headerlinesIn  = 1;
+format long;
+
+airfoil_struct = importdata(filename,delimiterIn,headerlinesIn);
+
+fields = fieldnames(airfoil_struct);
+coord  = char(fields(1));
+
+airfoil_coord = airfoil_struct.(coord);
